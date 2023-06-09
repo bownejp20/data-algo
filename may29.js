d90e8b3dbfc7d3aa1699f30e34b63e32b44d2177
bownejp20 <jbowne24@gmail.com>
2023-06-08 22:44:01 -0400

    leap code

--- may29.js
+++ may29.js
@@ -10,3 +10,39 @@ function funChallenge(input) {
   }
   return a; //O(1)
 }
+
+// BIG O(3 + 4n) => O(n)
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+  let a = 5; //O(1)
+  let b = 10; //O(1)
+  let c = 50; //O(1)
+  for (let i = 0; i < input; i++) { //O(n)
+    let x = i + 1;//O(n)
+    let y = i + 2;//O(n)
+    let z = i + 3;//O(n)
+
+  }
+  for (let j = 0; j < input; j++) {//O(n)
+    let p = j * 2;//O(n)
+    let q = j * 2;//O(n)
+  }
+  let whoAmI = "I don't know";//O(1)
+}
+
+//BIG O(4 + 7n) => O(n)
+
+function findTtl(arr, n){
+  for(let i = 0; i < arr.length; i ++){
+    for(let j = 1 + i; j < arr.length; j++){
+      if(arr[i] + arr[j] === n){
+        return [i, j]
+      }
+    }
+  }
+     }
+   
+ 
+ 
+ console.log(findTtl([1,2,3,4], 3))
