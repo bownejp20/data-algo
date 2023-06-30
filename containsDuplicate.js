6178e26e0ee3bd8d5a7058463dfb4281f99c56a0
bownejp20 <jbowne24@gmail.com>
2023-06-30 10:47:36 -0400

    contain duplicate nest loop

---
+++ containsDuplicate.js
@@ -0,0 +1,16 @@
+const containsDuplicate = (nums) => {
+  for(let i = 0;i < nums.length; i++){
+      for(let j = i +1; j < nums.length; j++){
+          if(nums[i] === nums[j]){
+              return true
+      }
+      }
+  }
+  return false
+  
+};
+
+containsDuplicate([1,1,1,3,3,4,3,2,4,2])
+
+
+
