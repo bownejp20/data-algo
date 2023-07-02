5163225922d3e4f5edc3685f0432d63e9d251be1
bownejp20 <jbowne24@gmail.com>
2023-07-01 21:42:22 -0400

    contains duplicate counting way

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -12,5 +12,19 @@ const containsDuplicate = (nums) => {
 
 containsDuplicate([1,1,1,3,3,4,3,2,4,2])
 
+const containsDuplicate = (nums) => {
+  const count = {}
+  for(let i = 0; i < nums.length; i++){
+   if(count[nums[i]]){
+     console.log(count)
+    return true  
+  }
+    count[nums[i]] = count[nums[i]]? count[nums[i]] +1 : 1
+  }
+  return false
+   }
+ containsDuplicate([1,1,1,3,3,4,3,2,4,2])
+ 
+
 
 
